71c7c76d423551cf87fd4a6cb2e944a7a6ba1112
Andrei <andrei.nekrasch@yandex.ru>
2024-10-07 22:35:22 +0300

    s

--- public/index.js
+++ public/index.js
@@ -2,8 +2,7 @@ async function add(event)
 {
     event.preventDefault();
     let item=document.querySelector("#addItem").value; 
-    console.log(item);
-
+    
     try {
         const response = await fetch('/add', {
             method: 'POST',
@@ -29,3 +28,34 @@ async function add(event)
 }
 
 document.querySelector("#seller").addEventListener("submit", add);
+
+async function select(event)
+{
+    event.preventDefault();
+    let item=document.querySelector("#choose").value; 
+    
+    try {
+        const response = await fetch('/select', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({  item })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        
+         
+        document.querySelector("#customerMessage").style.color="green";
+        document.querySelector("#customerMessage").innerHTML = `Response from server: ${JSON.stringify(data.result, null, 2)}`;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+document.querySelector("#customer").addEventListener("submit", select);
--- server.js
+++ server.js
@@ -24,6 +24,21 @@ app.post('/add', (req, res) => {
       }
 });
 
+app.post('/select', (req, res) => {
+  try{const selection =req.body;
+    console.log(selection);
+    
+    
+    
+    // console.log("Goods array: " + JSON.stringify(goods, null, 2))
+    // res.json({result})
+  }
+       catch (error) {
+        console.error('Error in /number route:', error.message);
+        res.status(400).json({ error: error.message });
+      }
+});
+
 
 
 app.listen(port, () => {
